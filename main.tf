5c91a2eb28079f7016c42598e4bd78d61cc2e566
Venkata-Teja-ML <136301901+Venkata-Teja-ML@users.noreply.github.com>
2023-08-24 16:33:30 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,12 +1,22 @@
-#new instance
-provider "aws" {
-    region = "us-west-2"
-}
-resource "aws_instance" "new_instance" {
-    ami = "ami-09dd2e08d601bff67"
-    instance_type = "t2.micro"
-    tags = {
-        Name = "HelloWorld"
-        Name = var.instance_name
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    local = {
+        source = "hashicorp/local"
+        version = "~>2.0"
     }
+  }
+}
+resource "local_file" "literature" {
+    filename = "art_of_war.txt"
+    content = <<-EOT
+    Sun Tzu said: The art of war is of vital importance to the State.
+
+    It is a matter of life and death, a road either to safety or to 
+    ruin. Hence it is a subject of inquiry which can on no account be
+    neglected.
+    add some more lines
+    add another some more lines
+
+    EOT
 }
